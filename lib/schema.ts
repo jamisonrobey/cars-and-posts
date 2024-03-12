6c12e7b6d9771810ca1e31a981d3c5776b333a8d
jamison <jamisonrobey@gmail.com>
2024-03-12 13:17:21 +1000

    update to schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,8 +1,49 @@
-import { mysqlTable, varchar, int } from 'drizzle-orm/mysql-core';
+import {
+  mysqlTable,
+  boolean,
+  varchar,
+  int,
+  mysqlEnum,
+  text,
+  date,
+} from "drizzle-orm/mysql-core";
 
-export const cars = mysqlTable('cars', {
-      car_id: int('car_id'),
-        make: varchar('make', { length: 50 }),
-          model: varchar('model', { length: 50 }),
-            body_type: varchar('body_type', { length: 50 }),
+export const cars = mysqlTable("cars", {
+  carId: int("car_id").primaryKey().autoincrement().notNull(),
+  make: varchar("make", { length: 50 }).notNull(),
+  model: varchar("model", { length: 50 }).notNull(),
+  year: int("year").notNull(),
+  vin: varchar("vin", { length: 50 }).notNull(),
+  bodyType: mysqlEnum("body_type", [
+    "SUV",
+    "Sedan",
+    "Hatchback",
+    "Coupe",
+    "Convertible",
+    "Wagon",
+    "Van",
+    "Ute",
+  ]).notNull(),
+  engineConfig: mysqlEnum("engine_config", [
+    "I4",
+    "I6",
+    "V6",
+    "V8",
+    "V10",
+    "V12",
+  ]).notNull(),
+  colour: varchar("colour", { length: 50 }).notNull(),
+  imagePath: varchar("image_path", { length: 50 }).notNull(),
+  trim: varchar("trim", { length: 50 }),
+  interiorColour: varchar("interior_colour", { length: 50 }),
+});
+
+export const posts = mysqlTable("posts", {
+  postId: int("post_id").primaryKey().autoincrement().notNull(),
+  carId: int("car_id").references(() => cars.carId),
+  title: varchar("title", { length: 50 }).notNull(),
+  description: text("description").notNull(),
+  uploadDate: date("upload_date").notNull(),
+  sold: boolean("is_sold").default(false),
+  views: int("views").default(0),
 });
