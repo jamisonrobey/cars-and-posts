acffdde8edb0327008fc17a1d2fe790d726b8af7
jamison <jamisonrobey@gmail.com>
2024-03-20 14:04:46 +1000

    fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,16 @@
+import { Fugaz_One } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+export const logoFont = Fugaz_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const sans = Open_Sans({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const boldSans = Open_Sans({
+  weight: ["600"],
+  subsets: ["latin"],
+});
