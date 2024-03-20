ae2b37acc244d615e221d4885a53de69063b45c7
jamison <jamisonrobey@gmail.com>
2024-03-20 14:06:01 +1000

    rename interfaces

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -1,11 +1,11 @@
-export interface InterfaceMakesAndModels {
+export interface MakesAndModels {
   [key: string]: {
     models: string[];
     count: number;
   };
 }
 
-export interface InterfaceSelectItem {
+export interface SelectItem {
   value: string;
   label: string;
   disabled?: boolean;
