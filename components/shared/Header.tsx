e7a52ce5c492ff9ee2e8edb89c3f804234318f77
jamison <jamisonrobey@gmail.com>
2024-03-23 21:50:03 +1000

    Styling: Header styling

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,21 +3,24 @@ import { logoFont, sans } from "../../lib/fonts";
 
 export const Header = () => {
   return (
-    <div className="flex items-center justify-between p-2 sm:p-16 z-10 sm:justify-center">
-      <h1 className={`${logoFont.className} text-center text-accent text-3xl`}>
+    <div className="flex items-center justify-between p-2 sm:p-16 z-10 sm:justify-center  mb-16">
+      <Link
+        href="/"
+        className={`${logoFont.className} text-center text-accent text-3xl sm:text-6xl`}
+      >
         Carmart.
-      </h1>
+      </Link>
       <div
-        className={`${sans.className} flex items-center w-3/4 justify-end text-lg space-x-4 sm:justify-end sm:space-x-16`}
+        className={`${sans.className} flex items-center w-3/4 justify-end text-lg sm:text-2xl space-x-4 sm:justify-end sm:space-x-16`}
       >
         <Link
-          className="hover:border-b-2 hover:border-accent duration-75 "
-          href={"/"}
+          className="hover:border-b-4 hover:border-accent duration-75 "
+          href={"/search/all"}
         >
           All Vehicles
         </Link>
         <Link
-          className="hover:border-b-2 hover:border-accent duration-75"
+          className="hover:border-b-4 hover:border-accent duration-75"
           href={"/"}
         >
           About
